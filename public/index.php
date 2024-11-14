fa7a329ad908b71bbf4260886b2dbf68967f918d
Lychtael <jitabilhaq12@gmail.com>
2024-11-14 22:39:47 +0700

    edit index

--- public/index.php
+++ public/index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello world</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus ad rem fugit fugiat voluptatum odio deserunt aliquam pariatur corrupti? Harum placeat exercitationem assumenda minima doloribus dicta rem doloremque molestias? Doloremque saepe nesciunt autem optio qui nostrum culpa necessitatibus aut sint unde? Architecto amet quasi est explicabo minus nulla, rem, quas dolores mollitia blanditiis excepturi. Architecto iusto, ratione adipisci totam quia, unde perferendis quos, non qui commodi cupiditate eaque eos voluptatum impedit corporis repellat at ab labore dignissimos nostrum possimus. Similique magnam impedit aliquam dolore ab? Distinctio voluptas odio, dolore nesciunt mollitia temporibus at, sint dignissimos corrupti voluptatem quisquam voluptatibus dolorem?</p>
 </body>
 </html>
