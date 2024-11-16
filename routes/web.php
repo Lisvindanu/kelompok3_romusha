3cd7faf27cbdc36cb05494ed4edaa55aa1cc0947
Lychtael <jitabilhaq12@gmail.com>
2024-11-16 08:39:59 +0700

    initial UI + routes

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', ['title'=>'HOME']);
+});
+Route::get('/about', function () {
+    return view('about', ['nama' =>'RetroGame-hub'],['title' =>'About']);
 });
