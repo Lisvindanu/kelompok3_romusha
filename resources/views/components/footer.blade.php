3fcbfa5b06eb349328a9f6560aacd8df1a3a50f5
Mohzu <mochzuhdi04@gmail.com>
2024-11-29 10:27:22 +0700

    Landing Page - Footer Section (revisi 1)

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,39 +1,30 @@
 <footer class="bg-yellow-400 text-red-700 py-8">
     <div class="container mx-auto px-4 text-center">
         <!-- Logo or Title -->
-        <h2 class="text-2xl font-bold mb-4">Retro Game Hub</h2>
+        <h2 class="text-2xl font-bold mb-4 font-pixelify">Retro Game Hub</h2>
 
         <!-- Navigation Links -->
         <div class="flex justify-center space-x-6 mb-6">
-            <a href="/" class="hover:underline">Home</a>
-            <a href="/about" class="hover:underline">About</a>
-            <a href="/games" class="hover:underline">Games</a>
-            <a href="/console" class="hover:underline">Console</a>
+            <a href="/" class="hover:underline font-pixelify">Home</a>
+            <a href="/about" class="hover:underline font-pixelify">About</a>
+            <a href="/games" class="hover:underline font-pixelify">Games</a>
+            <a href="/console" class="hover:underline font-pixelify">Console</a>
         </div>
 
         <!-- Social Media Icons -->
         <div class="flex justify-center space-x-4 mb-4">
             <a href="#" class="hover:opacity-75" aria-label="Facebook">
-                <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
-                    <path
-                        d="M22 12a10 10 0 1 0-11.5 9.9v-7H8v-3h2.5v-2c0-2.5 1.5-3.9 3.8-3.9 1.1 0 2.2.2 2.2.2v2.4h-1.2c-1.2 0-1.6.8-1.6 1.6v1.8h2.8l-.5 3H13v7A10 10 0 0 0 22 12z" />
-                </svg>
+                <i class="fab fa-facebook-f text-xl"></i>
             </a>
             <a href="#" class="hover:opacity-75" aria-label="Twitter">
-                <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
-                    <path
-                        d="M24 4.56c-.89.39-1.85.65-2.86.77a4.92 4.92 0 0 0 2.15-2.71 9.86 9.86 0 0 1-3.12 1.2 4.92 4.92 0 0 0-8.38 4.48A13.97 13.97 0 0 1 1.67 3.15a4.92 4.92 0 0 0 1.52 6.57 4.9 4.9 0 0 1-2.23-.61v.06a4.92 4.92 0 0 0 3.95 4.83c-.45.12-.93.18-1.42.18-.35 0-.69-.03-1.03-.1a4.92 4.92 0 0 0 4.6 3.42A9.87 9.87 0 0 1 0 19.54 13.93 13.93 0 0 0 7.55 22c9.05 0 14-7.5 14-14 0-.21 0-.43-.02-.64A10.01 10.01 0 0 0 24 4.56z" />
-                </svg>
+                <i class="fab fa-twitter text-xl"></i>
             </a>
             <a href="#" class="hover:opacity-75" aria-label="Instagram">
-                <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
-                    <path
-                        d="M16 1H8a7 7 0 0 0-7 7v8a7 7 0 0 0 7 7h8a7 7 0 0 0 7-7V8a7 7 0 0 0-7-7zM22 16a6 6 0 0 1-6 6H8a6 6 0 0 1-6-6V8a6 6 0 0 1 6-6h8a6 6 0 0 1 6 6zm-5-3a5 5 0 1 0-10 0 5 5 0 0 0 10 0zM19 5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z" />
-                </svg>
+                <i class="fab fa-instagram text-xl"></i>
             </a>
         </div>
 
         <!-- Copyright -->
-        <p class="text-sm">© 2024 RetroGameHub | All rights reserved.</p>
+        <p class="text-sm font-pixelify">© 2024 RetroGameHub | All rights reserved.</p>
     </div>
 </footer>
