a03b16f03063e57c827df85924d74fb3dc6dd7cf
Lyramor <mmarsa2435@gmail.com>
2024-12-29 02:11:19 +0700

    wip

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,4 +1,4 @@
-{{--@php dd($userData, $categories, $genres); @endphp--}}
+{{-- @php dd($userData, $categories, $genres); @endphp --}}
 <x-layout-dashboard>
     <div class="flex flex-col min-h-screen md:flex-row">
         <!-- Sidebar -->
@@ -7,24 +7,17 @@
         <!-- Konten Utama -->
         <main class="flex-1 p-6">
             <div class="flex justify-between items-center border-b pb-4 mb-4">
-                <h1 class="text-2xl font-semibold">Selamat Datang Kembali, {{$userData ['username']}}</h1>
+                <h1 class="text-2xl font-semibold">Selamat Datang Kembali, {{ $userData['username'] }}</h1>
             </div>
 
             <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-                <!-- Box Produk Game -->
-                <div class="bg-gray-800 rounded-lg p-6 flex flex-col items-center justify-between shadow-lg">
-                    <i class="fas fa-gamepad fa-5x text-gray-400 mb-4"></i>
-                    <h3 class="text-gray-200 text-xl font-bold">Produk Game</h3>
-                    <p class="text-gray-400 text-lg">10 Produk</p>
-                    <a href="/dashboard/product-game" class="text-blue-500 hover:text-blue-400 no-underline">Lihat Detail</a>
-                </div>
-
-                <!-- Box Produk Console -->
+                <!-- Box Produk -->
                 <div class="bg-gray-800 rounded-lg p-6 flex flex-col items-center justify-between shadow-lg">
                     <i class="fas fa-tv fa-5x text-gray-400 mb-4"></i>
-                    <h3 class="text-gray-200 text-xl font-bold">Produk Console</h3>
+                    <h3 class="text-gray-200 text-xl font-bold">Produk</h3>
                     <p class="text-gray-400 text-lg">10 Produk</p>
-                    <a href="/dashboard/product-console" class="text-blue-500 hover:text-blue-400 no-underline">Lihat Detail</a>
+                    <a href="/dashboard/product-game" class="text-blue-500 hover:text-blue-400 no-underline">Lihat
+                        Detail</a>
                 </div>
 
                 <!-- Box Genre -->
@@ -32,7 +25,8 @@
                     <i class="fas fa-layer-group fa-5x text-gray-400 mb-4"></i>
                     <h3 class="text-gray-200 text-xl font-bold">Genre</h3>
                     <p class="text-gray-400 text-lg">10 Genre</p>
-                    <a href="/dashboard/genre-game" class="text-blue-500 hover:text-blue-400 no-underline">Lihat Detail</a>
+                    <a href="/dashboard/genre-game" class="text-blue-500 hover:text-blue-400 no-underline">Lihat
+                        Detail</a>
                 </div>
 
                 <!-- Box Kategori -->
